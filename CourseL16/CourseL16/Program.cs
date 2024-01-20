25a8c844a4e8a92602a42ff4692f3d59aded5b6b
Humeniuk-Vasyl <48986399+Humeniuk-Vasyl@users.noreply.github.com>
2024-01-20 21:17:50 +0200

    L16
    Tasks for L16

---
+++ CourseL16/CourseL16/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using static System.Console;
+using static System.Math;
+
+namespace CourseL16
+{
+    public class Program
+    {
+        static Random rnd = new();
+
+        public static void Main()
+        {
+            /*//Ex1
+            MyDelegateClass.MyDelegate myDelegateEx = (a, b, c) => Round((double)(a + b + c) / 3, 3);
+            WriteLine($"res -> {myDelegateEx(2, 3, 9)}");
+            */
+
+            //Ex2
+            //MyOps.Ex2Test();
+
+            //Ex3_L16
+            Ex3(new Func<double, double>[10]);
+        }
+        public static void Ex3(Func<double, double>[] delegates)
+        {
+            List<double> items = new();
+            int length = delegates.Length;
+            WriteLine("Average of delegates that contain method with rnd nums =>");
+            for (int i = 0; i < length; i++)
+            {
+                delegates[i] = a => a;
+                items.Add(delegates[i](rnd.Next(10, 100)));
+                Write(!(i==length-1)?$"{items[i]} + " : $"{ items[i]} ");
+            }
+            WriteLine($"= {Round(items.Average(), 3)}");
+        }
+    }
+
+    //Ex1_L16
+    public class MyDelegateClass
+    {
+        public delegate double MyDelegate(int arg1, int arg2, int arg3);
+    }
+
+    //Ex2_L16
+    public class MyOps
+    {
+        //Op lyamda operations
+        private static readonly Func<double, double, double> add = (a, b) => a + b;
+        private static readonly Func<double, double, double> sub = (a, b) => a - b;
+        private static readonly Func<double, double, double> mult = (a, b) => a * b;
+        private static readonly Func<double, double, double> div = (a, b) => a / b;
+
+        public static void Ex2Test()
+        {
+            bool end = false;
+            double a = 0, b = 0;
+            string? op = "", _a, _b;
+            while (!end)
+            {
+                while (true)
+                {
+                    Write("Enter a: ");
+                    _a = ReadLine();
+                    Write("Enter b: ");
+                    _b = ReadLine();
+                    if (double.TryParse(_a, out a) && double.TryParse(_b, out b))
+                        break;
+                    WriteLine("a or b wasn't parsed. Plz enter again");
+                }
+                WriteLine("Choose operation: +, -, *, / or 0 for exit.");
+
+            OpSelector:
+                op = ReadLine();
+                switch (op)
+                {
+                    case "0": end = true; break;
+                    case "+": WriteLine($"add res -> {add(a, b)}"); break;
+                    case "-": WriteLine($"sub res -> {sub(a, b)}"); break;
+                    case "*": WriteLine($"mult res -> {mult(a, b)}"); break;
+                    case "/": WriteLine((b == 0) ? "Can't divide by 0!" : $"div res -> {div(a, b)}"); break;
+                    default:
+                        WriteLine("Operation unfound. Plz enter else sign from existing:\n" +
+                            "+ - * / or 0 for exit.");
+                        goto OpSelector;
+                }
+            }
+        }
+    }
+}
---
+++ CourseL16/CourseL16_T4/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace CourseL16_T4
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow() =>
+            InitializeComponent();
+
+        // StopWatch використовується для збереження та виводу результату про поточний час таймера
+        //DispatcherTimer використовується для підписки на подію Tick
+        //(з методом T1_Tick, що власне виводить час в TimeBox) та з інтервалом в 1 мілісекунду
+        Stopwatch s1;
+        DispatcherTimer t1;
+        private void MainWindow1_Initialized(object sender, EventArgs e)
+        {
+            s1 = new();
+            t1 = new()
+            {
+                Interval = TimeSpan.FromMilliseconds(50)
+            };
+            t1.Tick += T1_Tick;
+            t1.Start();
+        }
+        private void T1_Tick(object sender, EventArgs e) =>
+            TimeBox.Text = ((double)s1.ElapsedMilliseconds / 1000).ToString();
+        // для більш-менш адекватного представлення секунд
+        private void StartBtn_Click(object sender, RoutedEventArgs e) => s1.Start();
+
+        private void StopBtn_Click(object sender, RoutedEventArgs e) => s1.Stop();
+
+        private void ResetBtn_Click(object sender, RoutedEventArgs e) => s1.Reset();
+
+        private void MainWindow1_Unloaded(object sender, RoutedEventArgs e) => t1.Tick -= T1_Tick;
+    }
+}
