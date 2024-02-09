03b4bbc33d628c691ffb4e1fd83b4f7ca70fe602
Humeniuk-Vasyl <48986399+Humeniuk-Vasyl@users.noreply.github.com>
2024-02-09 14:58:21 +0200

    L19
    Tasks for L19

---
+++ CourseL19/CourseL19/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+namespace CourseL19
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //Ex1:
+            // не впевнений що правильно інтерпритував Car_Library до Converter
+            // але подальша суть завдання в розгляді рефрексії тож норм)
+            /*typeof(Convertor).GetMethod("ConvertorInfo",
+                BindingFlags.NonPublic | BindingFlags.Instance)
+                .Invoke(new FarhenheitToCelsiusClass(20), null);*/
+            // Current value: 20°F
+        }
+    }
+
+
+}
---
+++ CourseL19/Ex2/Program.cs
@@ -0,0 +1,16 @@
+﻿using CourseL19;
+using System.Reflection;
+
+//Ex2:
+
+var CtF = new CelsiusToFarhenheitClass(200);
+MethodInfo? CtFTest = typeof(CelsiusToFarhenheitClass).GetMethod("Conversion",
+    BindingFlags.NonPublic | BindingFlags.Instance);
+CtFTest?.Invoke(CtF, null);
+// 200°C to °F=392°F
+
+typeof(FarhenheitToCelsiusClass)?.GetMethod("Conversion",
+BindingFlags.NonPublic | BindingFlags.Instance)?
+    .Invoke(new FarhenheitToCelsiusClass(-470), null);
+// Temperature can't be less than -460°F
+ 
